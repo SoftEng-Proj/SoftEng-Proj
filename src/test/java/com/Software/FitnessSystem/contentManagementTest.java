fa0bde36939fd4cd6bda4837f22187fa62c03e61
Muath Hassoun <moath.hassoun@gmail.com>
2024-12-08 22:32:38 +0200

    Admin Test Files

---
+++ src/test/java/com/Software/FitnessSystem/contentManagementTest.java
@@ -0,0 +1,36 @@
+package com.Software.FitnessSystem;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+class contentManagementTest {
+	@Given("I am on the {string} page")
+	public void i_am_on_the_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I review a wellness article, tip, or recipe")
+	public void i_review_a_wellness_article_tip_or_recipe() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I can approve or reject the submission")
+	public void i_can_approve_or_reject_the_submission() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I review user feedback and complaints")
+	public void i_review_user_feedback_and_complaints() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I should be able to handle them appropriately")
+	public void i_should_be_able_to_handle_them_appropriately() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
