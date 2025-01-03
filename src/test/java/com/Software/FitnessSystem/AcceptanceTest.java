d1eb0cae53935f33ff36c0e08f8e8788a6606586
Muath Hassoun <moath.hassoun@gmail.com>
2025-01-03 22:22:19 +0200

    Project finished successfully.

--- src/test/java/com/Software/FitnessSystem/AcceptanceTest.java
+++ src/test/java/com/Software/FitnessSystem/AcceptanceTest.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 		plugin = {"html:target/cucumber-report/report.html"},
 		monochrome = true,
 		snippets = CucumberOptions.SnippetType.CAMELCASE,
-		tags = "@MuathHassounTests or @AmmarShafiiTests or @AlaaAlawnehTests")
+		tags = "@AlaaAlawnehTestss")
 
 /*
  * @MuathHassounTests or @AmmarShafiiTests or @AlaaAlawnehTests
